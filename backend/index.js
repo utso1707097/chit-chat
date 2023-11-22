6bbc13b2ee33ee6c69e7c81732f99b1075abef4c
utso1707097 <utsomollik1707097@gmail.com>
2023-11-23 02:10:35 +0600

    first commit start working in backend

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    //No More Deprecation Warning Options in Mongoose 6
+    //- these are no longer supported options in Mongoose 6 - only use it with old versions
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false
+  })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log("Something went wrong", e);
+  });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is ready");
+});
