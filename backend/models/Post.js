0262918b0cc4d68f021a6c48bd892066be4c9a72
utso1707097 <utsomollik1707097@gmail.com>
2023-11-23 03:16:26 +0600

    added user and post model and routes without jwt

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 
-const PostSchema = mongoose.Schema({}, { timestamps: true });
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
